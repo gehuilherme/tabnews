963db3075f508633d3ffa29aaee1b70f099d45e0
Guilherme Henrique <32115850+gehuilherme@users.noreply.github.com>
2024-07-09 02:55:59 +0000

    Adiciona `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   return res.status(200).json({
     chave: "valor1",
     chave1: "valor2",
